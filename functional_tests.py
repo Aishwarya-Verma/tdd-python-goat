bbe579379aee0df344a7c9c55b9d46fbbff25be3
Aishwarya Verma <65234806+Aishwarya-Verma@users.noreply.github.com>
2025-06-19 05:22:41 +0000

    update selenium setup docker and test file

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+options = Options()
+options.headless = True     # Run Firefox in headless mode(no GUI)
+browser = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
+
+try:
+    browser.get('http://localhost:8000')
+    assert 'Django' in browser.title
+    browser.save_screeshot('test_artifacts/trial_scrnsht.png')
+    print("Screenshot saved at: test_artifacts/trial_scrnsht.png")
+
+finally:
+    browser.quit()
